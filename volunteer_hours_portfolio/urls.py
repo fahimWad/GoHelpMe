e2a8a5367ebdb487ce87541fba34317051f23579
Avinash Swain <aviswain1@gmail.com>
2024-05-19 11:34:04 -0700

    made the urls require login for volunteer portfolio pages

--- volunteer_hours_portfolio/urls.py
+++ volunteer_hours_portfolio/urls.py
@@ -1,10 +1,11 @@
 #Define URL Patterns for the volunteer_hours_portfolio here
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('portfolio/', views.portfolio, name="portfolio"),
-    path('create_entry/', views.createEntry, name="create_entry"),
-    path('update_entry/<str:pk>/', views.updateEntry, name="update_entry"),
-    path('delete_entry/<str:pk>/', views.deleteEntry, name="delete_entry"),
+    path('portfolio/',login_required(views.portfolio), name="portfolio"),
+    path('create_entry/', login_required(views.createEntry), name="create_entry"),
+    path('update_entry/<str:pk>/', login_required(views.updateEntry), name="update_entry"),
+    path('delete_entry/<str:pk>/', login_required(views.deleteEntry), name="delete_entry"),
 ]
