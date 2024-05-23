748258e9fbcbd792bce347ae1277ac3c343508fd
Palak Parikh <palak.parikh29@gmail.com>
2024-05-22 18:25:41 -0700

    Created Volunteer Registration and Posting Events Application

--- GoHelpMe/settings.py
+++ GoHelpMe/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'volunteer_events',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,9 @@ ROOT_URLCONF = 'GoHelpMe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "volunteer_events" / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- volunteer_events/admin.py
+++ volunteer_events/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Event, EventRegistration
 
-# Register your models here.
+# Register the Event model with the admin site
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'time', 'location', 'organizer', 'max_attendees', 'current_attendees')
+    search_fields = ('name', 'location', 'organizer__username')
+    list_filter = ('date', 'location', 'organizer')
+    date_hierarchy = 'date'
+
+admin.site.register(Event, EventAdmin)
+
+# Register the EventRegistration model with the admin site
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('event', 'user', 'registration_date')
+    search_fields = ('event__name', 'user__username')
+    list_filter = ('event', 'user')
+
+admin.site.register(EventRegistration, EventRegistrationAdmin)
+
