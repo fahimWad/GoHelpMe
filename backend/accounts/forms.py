ec367f9c1885d00a43807b8937845de6c1213f1b
guydeong <Mjluzuriaga109@gmail.com>
2024-05-25 22:09:29 -0700

    Implemented Full Working Version of React App and DjangoRestAPI for Authentication System

---
+++ backend/accounts/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+
+from accounts.models import Account
+from .models import *
+
+class RegistrationForm(UserCreationForm):
+    # first_name = models.CharField(max_length=100, help_text="Required. Please add first name.")
+    # last_name = models.CharField(max_length=100, help_text="Required. Please add last name.")
+    # email = forms.EmailField(max_length=60, help_text = "Required. Add a valid email address.")
+    class Meta:
+        model = Account
+        fields = ("email", "username", "first_name", "last_name", "school", "password1", "password2")
+
+class AccountAuthenticationForm(forms.ModelForm):
+    # password = forms.CharField(Label='Password', widget = forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = {'username', 'password'}
+    
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        if not authenticate(username=username, password=password):
+            raise forms.ValidationError("Invalid Login")
+
---
+++ backend/volunteer_events/urls.py
@@ -0,0 +1,11 @@
+#Define URL Patterns for the volunteer_hours_portfolio here
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('post_event/', views.post_event, name='post_event'),  # URL for posting a new event
+    path('events/', views.event_list, name='event_list'),  # URL for listing events
+    path('events/<int:event_id>/', views.event_detail, name='event_detail'),  # URL for event details
+    path('events/<int:event_id>/register/', views.register_for_event, name='register_for_event'),  # URL for event registration
+    path('profile/', views.user_profile, name='user_profile'),  # URL for user profile
+]
