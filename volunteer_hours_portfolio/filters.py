164d0c774311788e3281d028f208041a84ee675e
ycho1908 <yehyeoncho@gmail.com>
2024-05-27 16:13:33 -0700

    added date input widget to search feature

--- volunteer_hours_portfolio/filters.py
+++ volunteer_hours_portfolio/filters.py
@@ -1,12 +1,13 @@
 import django_filters
 from django_filters import DateFilter
+from django.forms import DateInput
 
 from .models import *
 
 class VolunteerFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name="date", lookup_expr='gte', label='Start Date')
-    end_date = DateFilter(field_name="date", lookup_expr='lte', label='End Date')
-    
+    start_date = DateFilter(field_name="date", lookup_expr='gte', label='Start Date', widget=DateInput(attrs={'type':'date'}))
+    end_date = DateFilter(field_name="date", lookup_expr='lte', label='End Date', widget=DateInput(attrs={'type':'date'}))
+
     class Meta:
         model = Entry
         fields = '__all__'
