a690fff2d08e5a8a71ceff3b3458166c49ac0c95
Avinash Swain <aviswain1@gmail.com>
2024-05-28 19:57:41 -0700

    NOT WORKING

--- backend/volunteer_hours_portfolio/serializers.py
+++ backend/volunteer_hours_portfolio/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Entry
+from ..accounts.serializers import UserSerializer
 
 class EntrySerializer(serializers.ModelSerializer):
+    user = UserSerializer
     class Meta:
         model = Entry
         fields = "__all__"
